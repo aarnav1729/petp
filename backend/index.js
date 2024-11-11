fa6dac8f2e1039447890b52d8f2c4b93033caf2e
aarnav1729 <aarnavsingh836@gmail.com>
2024-11-11 14:04:12 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -2347,6 +2347,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-
 // start server
 const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
